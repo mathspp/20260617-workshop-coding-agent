9b96f92a149b1496c8db9adf6d430358211a14d1
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2026-06-17 16:41:11 +0100

    Auto sync commit

--- main_02.py
+++ main_02.py
@@ -1,8 +1,9 @@
 """
 Add the command `/exit` to stop the agent.
+Add a slash command `/rewind IDX` that “rewinds” the context
+by keeping only the messages `context[:idx]`.
+You may want to reprint the chat to update it.
 Prepend each message (yours or the agent's) with the index.
-Add a slash command `/rewind IDX` that “rewinds” the context by keeping only
-the messages `context[:idx]`. You may want to reprint the chat to update it.
 """
 
 from anthropic import Anthropic

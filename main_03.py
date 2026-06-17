85aab66473ab851dd77e2c6c40968ef745b31e8b
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2026-06-17 17:07:37 +0100

    Auto sync commit

---
+++ main_03.py
@@ -0,0 +1,55 @@
+from anthropic import Anthropic
+
+TOOL_INSTRUCTIONS = (
+    "If you need to read the contents of a "
+    + "file, reply with the exact string "
+    + "'tool_call: read('path/to/file')' "
+    + "and I'll send you the contents of the file back."
+)
+
+client = Anthropic()
+
+context = [
+    {
+        "role": "user",
+        "content": TOOL_INSTRUCTIONS,
+    }
+]
+
+while True:
+    user_message = input(" >>> ").strip()
+
+    if user_message.startswith("/"):
+        if user_message.startswith(("/quit", "/exit")):
+            break
+        elif user_message.startswith("/rewind"):
+            ...
+        else:
+            raise RuntimeError(f"Unknown command {user_message}.")
+        continue
+
+    context.append(
+        {
+            "role": "user",
+            "content": user_message,
+        }
+    )
+
+    response = client.messages.create(
+        max_tokens=1024,
+        messages=context,
+        model="claude-haiku-4-5",
+    )
+
+    print(response.content)
+    for block in response.content:
+        if block.type == "text":
+            print(block.text)
+            context.append(
+                {
+                    "role": "assistant",
+                    "content": block.text,
+                }
+            )
+        else:
+            raise RuntimeError(f"Can't handle block type {block.type}.")

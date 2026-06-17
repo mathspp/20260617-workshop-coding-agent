829623fa29b32a043bbe8fd91706850db043a758
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2026-06-17 16:14:42 +0100

    Auto sync commit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from anthropic import Anthropic
 
 client = Anthropic()
+
+response = client.messages.create(
+    max_tokens=1024,
+    messages=[
+        {
+            "role": "user",
+            "content": "Tell me a joke.",
+        }
+    ],
+    model="claude-haiku-4-5",
+)
+print(response.content)
